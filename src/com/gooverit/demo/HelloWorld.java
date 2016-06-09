e0689bbaba47d28ad6e17ce9b379e8434158c07d
rangu.dl <rangu.dl@alibaba-inc.com>
2016-06-09 15:30:02 +0800

    Initial commit

---
+++ src/com/gooverit/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.gooverit.demo;
+
+import com.sun.tools.internal.xjc.model.CElementPropertyInfo;
+
+/**
+ * Created by dailin on 16/6/9.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
